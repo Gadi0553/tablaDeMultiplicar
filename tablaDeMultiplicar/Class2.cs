950ad9bce4443adf291d8304430fa8ed1146ec23
scpty <scpty@LAPTOP-01H1J0B7>
2024-01-20 21:10:52 -0400

    Agregar archivos de proyecto.

---
+++ tablaDeMultiplicar/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tablaDeMultiplicar
+{
+    internal class Class2
+    {
+    }
+}
---
+++ tablaDeMultiplicar/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace tablaDeMultiplicar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número entre 0 y 255 para mostrar la tabla de multiplicar:\n");
+
+           
+            if (byte.TryParse(Console.ReadLine(), out byte numero))
+            {
+              
+                MostrarTablaMultiplicar(numero);
+            }
+            else
+            {
+                Console.WriteLine("Entrada inválida. Por favor, ingrese un número válido entre 0 y 255.");
+            }
+        }
+
+        static void MostrarTablaMultiplicar(byte numero)
+        {
+            Console.WriteLine($"\nTabla de Multiplicar del {numero}:\n");
+
+           
+            for (int i = 1; i <= 12; i++)
+            {
+                int producto = numero * i;
+                Console.WriteLine($"{numero} x {i} = {producto}");
+            }
+        }
+
+    }
+
+}
